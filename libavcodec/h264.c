prajnashi/ffmpeg

commit e08be681ccb4be5b9f9dd5a38bd33162b5e1517b

refill cabac variables in 16bit steps, 3% faster get_cabac()
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@6578 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/h264.c
@@ -7420,7 +7420,7 @@ static int decode_slice(H264Context *h){
             }
             eos = get_cabac_terminate( &h->cabac );
 
-            if( ret < 0 || h->cabac.bytestream > h->cabac.bytestream_end + 1) {
+            if( ret < 0 || h->cabac.bytestream > h->cabac.bytestream_end + 2) {
                 av_log(h->s.avctx, AV_LOG_ERROR, "error while decoding MB %d %d, bytestream (%d)\n", s->mb_x, s->mb_y, h->cabac.bytestream_end - h->cabac.bytestream);
                 ff_er_add_slice(s, s->resync_mb_x, s->resync_mb_y, s->mb_x, s->mb_y, (AC_ERROR|DC_ERROR|MV_ERROR)&part_mask);
                 return -1;

libavcodec/cabac.h
@@ -29,7 +29,7 @@
 //#undef NDEBUG
 #include <assert.h>
 
-#define CABAC_BITS 8
+#define CABAC_BITS 16
 #define CABAC_MASK ((1<<CABAC_BITS)-1)
 
 typedef struct CABACContext{
@@ -302,7 +302,7 @@ static inline void renorm_cabac_decoder_once(CABACContext *c){
 #if 0
     //P3:683
     asm(
-        "lea -0x20000(%0), %2       \n\t"
+        "lea -0x2000000(%0), %2     \n\t"
         "shr $31, %2                \n\t"  //FIXME 31->63 for x86-64
         "shl %%cl, %0               \n\t"
         "shl %%cl, %1               \n\t"
@@ -311,7 +311,7 @@ static inline void renorm_cabac_decoder_once(CABACContext *c){
 #elif 0
     //P3:680
     asm(
-        "cmp $0x20000, %0           \n\t"
+        "cmp $0x2000000, %0         \n\t"
         "setb %%cl                  \n\t"  //FIXME 31->63 for x86-64
         "shl %%cl, %0               \n\t"
         "shl %%cl, %1               \n\t"
@@ -321,7 +321,7 @@ static inline void renorm_cabac_decoder_once(CABACContext *c){
     int temp2;
     //P3:665
     asm(
-        "lea -0x20000(%0), %%eax    \n\t"
+        "lea -0x2000000(%0), %%eax  \n\t"
         "cdq                        \n\t"
         "mov %0, %%eax              \n\t"
         "and %%edx, %0              \n\t"
@@ -334,7 +334,7 @@ static inline void renorm_cabac_decoder_once(CABACContext *c){
     int temp2;
     //P3:673
     asm(
-        "cmp $0x20000, %0           \n\t"
+        "cmp $0x2000000, %0         \n\t"
         "sbb %%edx, %%edx           \n\t"
         "mov %0, %%eax              \n\t"
         "and %%edx, %0              \n\t"
@@ -347,7 +347,7 @@ static inline void renorm_cabac_decoder_once(CABACContext *c){
     int temp2;
     //P3:677
     asm(
-        "cmp $0x20000, %0           \n\t"
+        "cmp $0x2000000, %0         \n\t"
         "lea (%0, %0), %%eax        \n\t"
         "lea (%1, %1), %%edx        \n\t"
         "cmovb %%eax, %0            \n\t"

commit 02e0c84882af34e333c7e40be4ade72b58060437

fix 16 CABAC_BIT * 10l bug introduced in r6578
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@7440 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/h264.c
@@ -6224,7 +6224,10 @@ decode_intra_mb:
         // FIXME The two following lines get the bitstream position in the cabac
         // decode, I think it should be done by a function in cabac.h (or cabac.c).
         ptr= h->cabac.bytestream;
-        if (h->cabac.low&0x1) ptr-=CABAC_BITS/8;
+        if(h->cabac.low&0x1) ptr--;
+        if(CABAC_BITS==16){
+            if(h->cabac.low&0x1FF) ptr--;
+        }
 
         // The pixels are stored in the same order as levels in h->mb array.
         for(y=0; y<16; y++){