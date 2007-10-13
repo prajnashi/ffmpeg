prajnashi/ffmpeg

commit 30d74c154a2c39d9fbe2707758aadf8e7d4e3592

output last frame if seq end code is encountered fixes video decoding of AC3TEST.vob closes issue19
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@9870 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -2160,6 +2160,10 @@ int ff_mpeg1_find_frame_end(ParseContext *pc, const uint8_t *buf, int buf_size)
                 pc->frame_start_found=1;
                 break;
             }
+            if(state == SEQ_END_CODE){
+                pc->state=-1;
+                return i+1;
+            }
         }
     }
 
@@ -2196,7 +2200,7 @@ static int mpeg_decode_frame(AVCodecContext *avctx,
     MpegEncContext *s2 = &s->mpeg_enc_ctx;
     dprintf(avctx, "fill_buffer\n");
 
-    if (buf_size == 0) {
+    if (buf_size == 0 || (buf_size == 4 && AV_RB32(buf) == SEQ_END_CODE)) {
         /* special case for last picture */
         if (s2->low_delay==0 && s2->next_picture_ptr) {
             *picture= *(AVFrame*)s2->next_picture_ptr;

commit 04bd7ac27bc223f03d119eff45fedc1971113739

Fix for r9870 Patch by Stefan Lucke <stefan <at> lucke.in-berlin.de> Subject: API breakage by r9870 Date: 2007-10-05 11:06:47 GMT http://thread.gmane.org/gmane.comp.video.ffmpeg.devel/57467
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@10720 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -2239,7 +2239,7 @@ static int mpeg_decode_frame(AVCodecContext *avctx,
 
             *data_size = sizeof(AVFrame);
         }
-        return 0;
+        return buf_size;
     }
 
     if(s2->flags&CODEC_FLAG_TRUNCATED){