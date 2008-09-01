prajnashi/ffmpeg

commit 6e81c4226dd4874cbf67c8ef77e6e5ff38d4d8d9

Warn the user about me_method values that are not supported. Fixes issue503
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@15135 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/motion_est.c
@@ -240,6 +240,10 @@ int ff_init_me(MpegEncContext *s){
         av_log(s->avctx, AV_LOG_ERROR, "ME_MAP size is too small for SAB diamond\n");
         return -1;
     }
+    if(s->me_method!=ME_ZERO && s->me_method!=ME_EPZS && s->me_method!=ME_X1){
+        av_log(s->avctx, AV_LOG_ERROR, "me_method is only allowed to be set to zero and epzs; for hex,umh,full and others see dia_size\n");
+        return -1;
+    }
 
     c->avctx= s->avctx;
 

commit 0f13b1755a309d84408f42312ec9e3c58f14b55c

1000l Fix snow regression test that i broke in r15135.
git-svn-id: file:///var/local/repositories/ffmpeg/trunk@15136 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/motion_est.c
@@ -240,7 +240,8 @@ int ff_init_me(MpegEncContext *s){
         av_log(s->avctx, AV_LOG_ERROR, "ME_MAP size is too small for SAB diamond\n");
         return -1;
     }
-    if(s->me_method!=ME_ZERO && s->me_method!=ME_EPZS && s->me_method!=ME_X1){
+    //special case of snow is needed because snow uses its own iterative ME code
+    if(s->me_method!=ME_ZERO && s->me_method!=ME_EPZS && s->me_method!=ME_X1 && s->avctx->codec_id != CODEC_ID_SNOW){
         av_log(s->avctx, AV_LOG_ERROR, "me_method is only allowed to be set to zero and epzs; for hex,umh,full and others see dia_size\n");
         return -1;
     }